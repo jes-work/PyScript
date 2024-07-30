c3c1ef64935dc29fddbb2ecc43990851d996dbed
jes-work <127836564+jes-work@users.noreply.github.com>
2024-07-30 13:49:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from pyscript import display
 from datetime import datetime
+from arrr import translate
+
 now = datetime.now()
 display(now.strftime("%m/%d/%Y, %H:%M:%S"))
+
+english = "Hello there. How are you?"
+pirate = translate(english)
+display(pirate)
